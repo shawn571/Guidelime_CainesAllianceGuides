c771376e76cef9900141eaa7c6ee0568588842d3
shawn571 <71139409+shawn571@users.noreply.github.com>
2021-05-20 02:31:59 -0400

    Update Azuremyst Isle to Bloodmyst Isle Draenei 1-20.lua

--- Guidelime_CainesAllianceGuides/Azuremyst Isle to Bloodmyst Isle Draenei 1-20.lua
+++ Guidelime_CainesAllianceGuides/Azuremyst Isle to Bloodmyst Isle Draenei 1-20.lua
@@ -326,9 +326,9 @@ Talk to Daelyshia to get the flight point.[P][G34.4,47.99,1Ashenvale]
 Talk to Raene Wolfrunner to turn in [QT10752 Onward to Ashenvale].
 Go here to head to the Barrens and up the mountain.[G69.1,86.9,2Ashenvale][G49,5.3,2The Barrens][G49.3,12.3,2The Barrens][G52.3,6.7The Barrens].
 Destroy the Hearthstone in your bag, then go to Help -> Character Stuck -> Teleport to Graveyard, then resurrect at the spirit healer.
-Talk to Innkeeper Heather to get a Hearthstone and make this inn your home.[S][G52.86,53.69,1Westfall]
 Talk to Thor to get the flight point.[P][G56.54,52.64,1Westfall]
 Talk to Dungar Longdrink to get the flight point.[G66.2,62.4,2Stormwind City][P]
+Talk to Innkeeper Allison to make this inn your home.[S][G52.73,65.62Stormwind City]
 Talk to Guard Parker to accept [QA244 Encroaching Gnolls].
 Talk to Deputy Feldon to turn in [QT244 Encroaching Gnolls] and accept [QA246 Assessing the Threat].
 Talk to Ariena Stormfeather to get the flight point.[P][G30.58,59.39,1Redridge Mountains]
@@ -360,4 +360,5 @@ Fly to [F Westfall]
 Talk to Grimbooze Thunderbrew to accept [QA117 Thunderbrew Lager]. This is repeatable so you will have to mark it complete.
 Talk to Grimbooze Thunderbrew again to turn in [QT117 Thunderbrew Lager] since you should have the Hops already. This is repeatable so you will have to mark it complete.
 The Keg of Thunderbrew you have is for [QC116,1 Dry Times]
+Fly to [F Redridge Mountains]
 ]], 'Caines Alliance Guides')
